c46c1b85ec850d26a08ba3878877efb9c98b62ab
haru90 <nakamigawa.h@gmail.com>
2020-08-16 23:55:26 +0900

    アラートの追加

--- react-app/src/components/EditCard.tsx
+++ react-app/src/components/EditCard.tsx
@@ -23,53 +23,57 @@ const EditCard = () => {
   // FlipListが親　データを取る必要がない
 
   return (
-    <Card>
-      <TextField placeholder={" オモテ"} style={{width:"100%"}}/>
-      <Divider />
-      <TextField placeholder={" ウラ"} style={{width:"100%"}}/>
-      <Divider />
-      <FormControl>
-        <InputLabel>LINE通知</InputLabel>
-        <Select
-        >
-          <MenuItem value="">
-            <em>通知なし</em>
-          </MenuItem>
-          <MenuItem value={10}>07:00〜</MenuItem>
-          <MenuItem value={20}>08:00〜</MenuItem>
-          <MenuItem value={30}>09:00〜</MenuItem>
-        </Select>
-      </FormControl>
-      <Divider />
-      <textarea cols={80} rows={9} placeholder={"メモ"} style={{margin:"10px"}}></textarea>
-      <Divider />
-        <Button
-          variant="contained"
-          startIcon={<CancelIcon />}
-          style={{margin:'5px 15px 5px 15px',width:"45%"}}
-        >
-          キャンセル
-        </Button>
-        <Button
-          variant="contained"
-          color="secondary"
-          startIcon={<DeleteIcon />}
-          style={{margin:'5px 15px 5px 15px',width:"45%"}}
-        >
-          削除
-        </Button>
-      <div>
-        <Button
-          variant="contained"
-          color="primary"
-          startIcon={<SaveIcon />}
-          style={{margin:'5px',
-          width:'98%'}}
-        >
-          保存
-        </Button>
-      </div>
-    </Card>
+    <form>
+      <Card>
+        <TextField placeholder={" オモテ"} style={{width:"100%"}}/>
+        <Divider />
+        <TextField placeholder={" ウラ"} style={{width:"100%"}}/>
+        <Divider />
+        <FormControl>
+          <InputLabel>LINE通知</InputLabel>
+          <Select
+          >
+            <MenuItem value="">
+              <em>通知なし</em>
+            </MenuItem>
+            <MenuItem value={10}>07:00〜</MenuItem>
+            <MenuItem value={20}>08:00〜</MenuItem>
+            <MenuItem value={30}>09:00〜</MenuItem>
+          </Select>
+        </FormControl>
+        <Divider />
+        <textarea cols={80} rows={9} placeholder={"メモ"} style={{margin:"10px"}}></textarea>
+        <Divider />
+          <Button
+            variant="contained"
+            startIcon={<CancelIcon />}
+            style={{margin:'5px 15px 5px 15px',width:"45%"}}
+          >
+            キャンセル
+          </Button>
+          <Button
+            type='submit'
+            variant="contained"
+            color="secondary"
+            startIcon={<DeleteIcon />}
+            style={{margin:'5px 15px 5px 15px',width:"45%"}}
+          >
+            削除
+          </Button>
+        <div>
+          <Button
+            type='submit'
+            variant="contained"
+            color="primary"
+            startIcon={<SaveIcon />}
+            style={{margin:'5px',
+            width:'98%'}}
+          >
+            保存
+          </Button>
+        </div>
+      </Card>
+    </form>
   );
 };
 
--- react-app/src/components/TitleBar.tsx
+++ react-app/src/components/TitleBar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {AppBar,Toolbar, Typography} from '@material-ui/core';
+import {AppBar, Toolbar, Typography} from '@material-ui/core';
+import { Alert, AlertTitle } from '@material-ui/lab';
 
 const TitleBar=()=> {
   return (
@@ -8,6 +9,14 @@ const TitleBar=()=> {
           <Typography variant="h6">
             Flip Lists
           </Typography>
+          <Alert severity="warning">
+            <AlertTitle>Warning</AlertTitle>
+            This is a warning alert — <strong>check it out!</strong>
+          </Alert>
+          <Alert severity="success">
+            <AlertTitle>Success</AlertTitle>
+            This is a success alert — <strong>check it out!</strong>
+          </Alert>
         </Toolbar>
       </AppBar>
   );
