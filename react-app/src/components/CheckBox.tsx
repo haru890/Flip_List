ba1d42770f1bce53b1937cbd6c2344bec0a0400a
haru90 <nakamigawa.h@gmail.com>
2020-09-02 23:00:12 +0900

    checkboxのチェックが外れている状態をdefaultに設定

--- react-app/src/components/CheckBox.tsx
+++ react-app/src/components/CheckBox.tsx
@@ -3,16 +3,9 @@ import Checkbox from '@material-ui/core/Checkbox';
 
 const CheckBox=()=> {
 
-  const [checked, setChecked] = React.useState(true);
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
-
   return (
     <>
       <Checkbox
-        defaultChecked
         color="primary"
         inputProps={{ 'aria-label': 'secondary checkbox' }}
       />
