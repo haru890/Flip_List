2c049c576504e8bc92124372958f8afefbea9a2e
haru90 <nakamigawa.h@gmail.com>
2020-09-05 23:47:33 +0900

    リファクタリング

--- react-app/src/components/Flips.tsx
+++ react-app/src/components/Flips.tsx
@@ -2,11 +2,8 @@ import React, { ReactNode, FC, useState, ChangeEvent, useEffect } from 'react';
 import {Card, AppBar, Tabs, Typography, Box, styled, Button} from '@material-ui/core';
 import { Tab as _Tab } from '../models/tab';
 import { fetchTabs } from '../api/tab';
-import CheckCircleIcon from '@material-ui/icons/CheckCircle';
-import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
 import DeleteAlertDialog from './DeleteAlertDialog';
 import CheckBox from './CheckBox';
-import TextChangeButton from './TextChangeButton';
 
 interface Props {
   children?: ReactNode;
@@ -52,62 +49,6 @@ const Flips=()=> {
 
   return (
     <>
-      <AppBar position="static" color="default">
-        {/* <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-        >
-          {tabs ? tabs.map((tab) => (
-              <Tab label={tab.tag}/>
-            )):null
-          }
-        </Tabs> */}
-        {/* <Tabs
-          value={value}
-          onChange={handleChange}
-          indicatorColor="primary"
-          textColor="primary"
-          variant="scrollable"
-          scrollButtons="auto"
-          aria-label="scrollable auto tabs example"
-        >
-          <Tab label="8/27 木" />
-          <Tab label="8/28 金" />
-          <Tab label="メモ" />
-          <Tab label="勉強" />
-          <Tab label="就活" />
-          <Tab label="仕事" />
-          <Tab label="日記" />
-          <Tab label="健康" />
-          <Tab label="家計簿" />
-        </Tabs> */}
-      </AppBar>
-      {/* <TabList/>
-      <TabPanel value={value} index={0}>
-        <FlipList/>
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        <FlipList/>
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        <FlipList/>
-      </TabPanel>
-      <TabPanel value={value} index={3}>
-        Item Four
-      </TabPanel>
-      <TabPanel value={value} index={4}>
-        Item Five
-      </TabPanel>
-      <TabPanel value={value} index={5}>
-        Item Six
-      </TabPanel>
-      <TabPanel value={value} index={6}>
-        Item Seven
-      </TabPanel> */}
     <Root>
       <Button variant="outlined" color="primary" style={{width:"93%", padding:"15px", margin: "30px 0px 0px 20px"}}>
         <CheckBox />フリップ1<DeleteAlertDialog />
@@ -121,23 +62,32 @@ const Flips=()=> {
       <Button variant="outlined" color="primary" style={{width:"93%", padding:"15px", margin: "30px 0px 0px 20px"}}>
         <CheckBox />フリップ4<DeleteAlertDialog />
       </Button>
-      <TextChangeButton variant="outlined" color="primary" style={{width:"93%", padding:"15px", margin: "30px 0px 30px 20px"}}>
+      <Select>
+      <Select>
+      <Button variant="outlined" color="primary" style={{width:"93%", padding:"15px", margin: "30px 0px 30px 20px"}}>
         <CheckBox />フリップ5<DeleteAlertDialog />
-      </TextChangeButton>
+      </Button>
+      </Select>
+      </Select>
     </Root>
-    
+    <Select><a>test</a></Select>
     </>
   );
 }
 
 const Root = styled(Card)({
-  // backgroundColor: "#e1f5fe",
+  backgroundColor: 'white',
 });
 
-// const Border = styled(Card)({
-//   borderCollapse: "collapse",
-//   border: "1px solid #cfd8dc",
-//   padding:"10px"
-// });
+const Select = styled(Button)({
+  '&:hover': {
+    color: '#FF0000',
+    borderColor: '#FF0000',
+  },
+  '&:active': {
+    color: '#FF0000',
+    borderColor: '#FF0000',
+  },
+});
 
 export default Flips;
