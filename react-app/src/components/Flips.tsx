b58f8802eea41ab5d1ee8724cd0c8173e2d9ac55
haru90 <nakamigawa.h@gmail.com>
2020-09-08 22:50:37 +0900

    accordion componentを使用するか検討中

--- react-app/src/components/Flips.tsx
+++ react-app/src/components/Flips.tsx
@@ -4,6 +4,15 @@ import { Tab as _Tab } from '../models/tab';
 import { fetchTabs } from '../api/tab';
 import DeleteAlertDialog from './DeleteAlertDialog';
 import CheckBox from './CheckBox';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionActions from '@material-ui/core/AccordionActions';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Chip from '@material-ui/core/Chip';
+import Divider from '@material-ui/core/Divider';
 
 interface Props {
   children?: ReactNode;
@@ -50,6 +59,48 @@ const Flips=()=> {
   const [selected, setSelected] = useState<boolean>(false);
 
 
+
+  const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: '100%',
+    },
+    heading: {
+      fontSize: theme.typography.pxToRem(15),
+    },
+    secondaryHeading: {
+      fontSize: theme.typography.pxToRem(15),
+      color: theme.palette.text.secondary,
+    },
+    icon: {
+      verticalAlign: 'bottom',
+      height: 20,
+      width: 20,
+    },
+    details: {
+      alignItems: 'center',
+    },
+    column: {
+      flexBasis: '33.33%',
+    },
+    helper: {
+      borderLeft: `2px solid ${theme.palette.divider}`,
+      padding: theme.spacing(1, 2),
+    },
+    link: {
+      color: theme.palette.primary.main,
+      textDecoration: 'none',
+      '&:hover': {
+        textDecoration: 'underline',
+      },
+    },
+  }),
+);
+
+  const classes = useStyles();
+
+
+
   return (
     <>
     <Root>
@@ -69,6 +120,48 @@ const Flips=()=> {
         <CheckBox />フリップ3<DeleteAlertDialog />
       </Button>
     </Root>
+
+
+    <div className={classes.root}>
+      <Accordion defaultExpanded>
+        <AccordionSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1c-content"
+          id="panel1c-header"
+        >
+          <div className={classes.column}>
+            <Typography className={classes.heading}>Location</Typography>
+          </div>
+          <div className={classes.column}>
+            <Typography className={classes.secondaryHeading}>Select trip destination</Typography>
+          </div>
+        </AccordionSummary>
+        <AccordionDetails className={classes.details}>
+          <div className={classes.column} />
+          <div className={classes.column}>
+            <Chip label="Barbados" onDelete={() => {}} />
+          </div>
+          <div className={clsx(classes.column, classes.helper)}>
+            <Typography variant="caption">
+              Select your destination of choice
+              <br />
+              <a href="#secondary-heading-and-columns" className={classes.link}>
+                Learn more
+              </a>
+            </Typography>
+          </div>
+        </AccordionDetails>
+        <Divider />
+        <AccordionActions>
+          <Button size="small">Cancel</Button>
+          <Button size="small" color="primary">
+            Save
+          </Button>
+        </AccordionActions>
+      </Accordion>
+    </div>
+  
+
     </>
   );
 }
